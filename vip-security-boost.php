3ba792e09de188c9ad54b3055f7b1a6316829ad1
Bruno Basto <b.basto@gmail.com>
2025-03-11 16:04:31 -0300

    Adds inactive users module

--- vip-security-boost.php
+++ vip-security-boost.php
@@ -1,4 +1,30 @@
 <?php declare(strict_types = 1);
 
-require_once __DIR__ . '/modules/auth-monitoring/auth-monitoring.php';
-require_once __DIR__ . '/modules/second-plugin/second-plugin.php';
+require_once __DIR__ . '/class-integration.php';
+
+use Automattic\VIP\Integrations\IntegrationsSingleton;
+use Automattic\VIP\Security\Integration;
+
+/**
+ * Local environment specific configurations.
+ */
+$is_local_env = ! defined( 'VIP_GO_APP_ENVIRONMENT' ) || 'local' === constant( 'VIP_GO_APP_ENVIRONMENT' );
+
+if ( $is_local_env ) {
+	if ( ! defined( 'VIP_GO_APP_ID' ) || ! constant( 'VIP_GO_APP_ID' ) ) {
+		define( 'VIP_GO_APP_ID', 101 );
+	}
+
+	define( 'GOOP_API_URL', vip_get_env_var( 'GOOP_API_URL', getenv( 'GOOP_API_URL' ) ) );
+
+    /**
+     * Register and activate the integration.
+     */
+    $integration = Integration::instance();
+
+    IntegrationsSingleton::instance()->register( $integration );
+    IntegrationsSingleton::instance()->activate_platform_integrations();
+
+    // Load the integration
+    $integration->load();
+}
