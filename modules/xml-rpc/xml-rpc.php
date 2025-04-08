abb3fd81d03edb6b97d78f4dae6b56567d3fe7a2
Bruno Basto <b.basto@gmail.com>
2025-04-08 09:47:12 -0300

    Completely disable XML-RPC by implementing the `wp_xmlrpc_server_class` filter
    `xmlrpc_enabled` only controls wether methods that require authentication are enabled. See https://developer.wordpress.org/reference/hooks/xmlrpc_enabled/

--- modules/xml-rpc/xml-rpc.php
+++ modules/xml-rpc/xml-rpc.php
@@ -11,7 +11,8 @@ class Xml_Rpc {
 		}
 
 		$configs        = constant( 'VIP_SECURITY_BOOST_CONFIGS' );
-		$xmlrpc_configs = $configs['xml-rpc'] ?? [];
+		$module_configs = $configs[ 'module_configs' ] ?? [];
+		$xmlrpc_configs = $module_configs['xml-rpc'] ?? [];
 		self::$disabled = $xmlrpc_configs['disabled'] ?? false;
 
 		if ( self::$disabled ) {
@@ -20,10 +21,31 @@ class Xml_Rpc {
 	}
 
 	/**
-	 * Disable XML-RPC by returning false to the xmlrpc_enabled filter.
+	 * Disable XML-RPC
 	 */
-	private static function disable_xml_rpc() {
+	public static function disable_xml_rpc() {
+		// Disable XML-RPC methods that require authentication.
 		add_filter( 'xmlrpc_enabled', '__return_false', PHP_INT_MAX );
+
+		// Remove the “Really Simple Discovery” link from the header.
+		remove_action( 'wp_head', 'rsd_link' );
+
+		// Remove the X-Pingback HTTP header.
+		add_filter( 'wp_headers', function( $headers ) {
+			if ( isset( $headers['X-Pingback'] ) ) {
+				unset( $headers['X-Pingback'] );
+			}
+			return $headers;
+		}, PHP_INT_MAX, 1 );
+
+		// Return an empty array for all XML-RPC methods.
+		add_filter( 'xmlrpc_methods', '__return_empty_array', PHP_INT_MAX );
+
+		// Disable XML-RPC completely by returning a 403 Forbidden header.
+		add_filter('wp_xmlrpc_server_class', function( $class ) {
+			header('HTTP/1.1 403 Forbidden');
+			exit('Access to XML-RPC is disabled on this site.');
+		});
 	}
 }
 
