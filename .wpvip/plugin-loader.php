87607ef6cc05c6208b409358ddea454f2a9d3d04
Katrina Tantay <katrina.tantay@automattic.com>
2025-04-10 12:18:26 -0500

    Remove uintended filters

--- .wpvip/plugin-loader.php
+++ .wpvip/plugin-loader.php
@@ -5,12 +5,3 @@ defined( 'ABSPATH' ) || die();
 if ( ! defined( 'WP_TESTS_DOMAIN' ) && function_exists( 'wpcom_vip_load_plugin' ) ) {
 	wpcom_vip_load_plugin( 'vip-security-boost/vip-security-boost.php' );
 }
-// add_filter( 'wpcom_vip_is_two_factor_forced', '__return_true' );
-// add_filter( 'wpcom_vip_is_two_factor_local_testing', '__return_true' );
-
-add_action( 'set_current_user', function() { 
-	$limited = current_user_can( 'edit_posts' );
-	add_filter( 'wpcom_vip_is_two_factor_forced', function() use ( $limited ) {
-			return $limited;
-	}, PHP_INT_MAX );
-} );
