c292b46867aee5d2a4aebf967f6509042973a514
Bruno Basto <b.basto@gmail.com>
2025-04-11 13:06:28 -0300

    Adds more integration tests for XML-RCP

--- utils/dev-env.php
+++ utils/dev-env.php
@@ -3,10 +3,19 @@
 namespace Automattic\VIP\Security\Utils;
 
 function load_integration_configs_from_url() {
+    if ( defined( 'WP_CLI' ) && constant( 'WP_CLI' ) ) {
+        return;
+    }
+
     $config_api_url = vip_get_env_var( 'VIP_CONFIG_API_URL', getenv( 'VIP_CONFIG_API_URL' ) );
-    $endpoint       = sprintf( '%s/integration?slug=security-boost&level=site&site_id=%s&is_vip=true', $config_api_url, constant( 'VIP_GO_APP_ID' ) );
-    $api_error      = new \WP_Error( 'config-api-error', 'There was an error while fetching the integration configuration from the API.' );
-    $response       = vip_safe_wp_remote_get( $endpoint, $api_error, 5, 5, 5, array() );
+
+    if ( ! $config_api_url ) {
+        return;
+    }
+
+    $endpoint  = sprintf( '%s/integration?slug=security-boost&level=site&site_id=%s&is_vip=true', $config_api_url, constant( 'VIP_GO_APP_ID' ) );
+    $api_error = new \WP_Error( 'config-api-error', 'There was an error while fetching the integration configuration from the API.' );
+    $response  = vip_safe_wp_remote_get( $endpoint, $api_error, 5, 5, 5, array() );
 
     if ( is_wp_error( $response ) ) {
         error_log( 'Error: ' . $response->get_error_message() );
@@ -28,6 +37,6 @@ function load_integration_configs_from_url() {
 }
 
 function load_integration_configs_from_headers() {
-    $configs = json_decode( $_SERVER['HTTP_X_INTEGRATION_TEST_CONFIGS'], true );
+    $configs = json_decode( base64_decode( $_SERVER['HTTP_X_INTEGRATION_TEST_CONFIGS'] ), true );
     define( 'VIP_SECURITY_BOOST_CONFIGS', $configs );
 }
