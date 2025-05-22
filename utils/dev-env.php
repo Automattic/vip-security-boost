296237d33e3e8545d2ed2bb84b6c7162e8ac1427
Katrina Tantay <katrina.tantay@automattic.com>
2025-05-22 11:37:58 -0500

    Remove wp-cli commented code and autoload

--- utils/dev-env.php
+++ utils/dev-env.php
@@ -3,11 +3,6 @@
 namespace Automattic\VIP\Security\Utils;
 
 function load_integration_configs_from_url() {
-	// Removed for testing user creation (i.e. notify-privileged-activity)
-	// if ( defined( 'WP_CLI' ) && constant( 'WP_CLI' ) ) {
-	//  return;
-	// }
-
 	$config_api_url = vip_get_env_var( 'VIP_CONFIG_API_URL', getenv( 'VIP_CONFIG_API_URL' ) );
 
 	if ( ! $config_api_url ) {
