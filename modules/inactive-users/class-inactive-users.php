0d933886474cdb1fb652a6832dddd46288619374
Bruno Basto <b.basto@gmail.com>
2025-06-26 10:03:07 -0300

    Move cache check to the top so we don't perform unnecessary queries

--- modules/inactive-users/class-inactive-users.php
+++ modules/inactive-users/class-inactive-users.php
@@ -82,6 +82,11 @@ class Inactive_Users {
 			return $user_id;
 		}
 
+		if ( wp_cache_get( $user_id, self::LAST_SEEN_CACHE_GROUP ) ) {
+			// Last seen meta was checked recently
+			return $user_id;
+		}
+
 		$user = get_userdata( $user_id );
 		if ( ! $user ) {
 			return $user_id;
@@ -89,11 +94,6 @@ class Inactive_Users {
 
 		if ( self::is_considered_inactive( $user_id ) ) {
 			// User needs to be unblocked first
-			return $user_id;
-		}
-
-		if ( wp_cache_get( $user_id, self::LAST_SEEN_CACHE_GROUP ) ) {
-			// Last seen meta was checked recently
 			return $user_id;
 		}
 
