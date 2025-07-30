6733be7e6f40feaea7f2927c17a275ce9be24db3
Bruno Basto <b.basto@gmail.com>
2025-07-30 17:28:52 -0300

    Remove filter after use

--- modules/highlight-mfa-users/class-highlight-mfa-users.php
+++ modules/highlight-mfa-users/class-highlight-mfa-users.php
@@ -102,6 +102,9 @@ class Highlight_MFA_Users {
 
 		$data['users_without_2fa_count'] = $users_without_2fa_count;
 
+		// Remove fix for unreliable FOUND_ROWS() query
+		remove_filter( 'found_users_query', [ Users_Query_Utils::class, 'fix_found_users_query' ], 10, 2 );
+
 		return $data;
 	}
 
