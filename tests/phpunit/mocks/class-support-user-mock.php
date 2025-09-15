14637051d44547373ca3b14c7e50c8945b81eaca
Joanne Lee <joanne.lee@automattic.com>
2025-09-15 01:19:49 -0700

    Use util from mu-plugins to check for A8C-owned email domains

--- tests/phpunit/mocks/class-support-user-mock.php
+++ tests/phpunit/mocks/class-support-user-mock.php
@@ -8,11 +8,25 @@ namespace Automattic\VIP\Support_User;
 if ( ! class_exists( '\Automattic\VIP\Support_User\User' ) ) {
 	class User {
 		public static $mock_vip_support_users = [];
-		
+
 		public static function user_has_vip_support_role( $user_id ) {
 			return in_array( $user_id, self::$mock_vip_support_users, true );
 		}
-		
+
+		public static function is_a8c_email( $email ) {
+			if ( ! is_email( $email ) ) {
+				return false;
+			}
+			list( , $domain ) = explode( '@', $email, 2 );
+			$a8c_domains      = array(
+				'a8c.com',
+				'automattic.com',
+				'matticspace.com',
+				'wpvip.com',
+			);
+			return in_array( $domain, $a8c_domains, true );
+		}
+
 		public static function reset_mock() {
 			self::$mock_vip_support_users = [];
 		}
--- utils/class-email-utils.php
+++ utils/class-email-utils.php
@@ -20,26 +20,4 @@ class Email_Utils {
 		}
 		return substr( $part, 1 ); // strip leading "@"
 	}
-
-	/** True if the email is on an Automattic/VIP-owned domain (filterable) */
-	public static function is_a8c_owned( $email ): bool {
-		$domain = self::domain( $email );
-		if ( null === $domain ) {
-			return false;
-		}
-
-		$allowed = apply_filters(
-			'vip_security_jetpack_owner_email_domains',
-			defined( Constants::class . '::A8C_EMAIL_DOMAINS' )
-				? Constants::A8C_EMAIL_DOMAINS
-				: [ 'wpvip.com', 'automattic.com' ]
-		);
-
-		$allowed = array_map(
-			static fn( $d ) => strtolower( trim( (string) $d ) ),
-			(array) $allowed
-		);
-
-		return in_array( $domain, $allowed, true );
-	}
 }
