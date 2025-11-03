4547ae05f0b99bccf3cd77e65bff41e9534d6d82
Andrea Grassi <andrea.grassi@automattic.com>
2025-11-03 12:52:01 +0100

    adds an helper to retrieve security boost configs

---
+++ tests/e2e/lib/security-boost-config-helper.ts
@@ -0,0 +1,112 @@
+export interface SecurityBoostConfig {
+	enabled_modules: string[];
+	available_modules: string[];
+	module_configs: {
+		'inactive-users': {
+			mode: string;
+			considered_inactive_after_days: number;
+			roles: string[];
+			capabilities: string[];
+			needs_review: boolean;
+		};
+		'forced-mfa-users': {
+			roles: string[];
+			capabilities: string[];
+			needs_review: boolean;
+		};
+		'xml-rpc': {
+			mode: string;
+			needs_review: boolean;
+		};
+		'session-control': {
+			expiration_days: number;
+			needs_review: boolean;
+		};
+		'highlight-mfa-users': {
+			roles: string[];
+			capabilities: string[];
+		};
+		'notify-privileged-activity': {
+			email_users: string[];
+		};
+	};
+}
+export const DEFAULT_CONFIG: SecurityBoostConfig = {
+	enabled_modules: [
+		'notify-privileged-activity',
+		'highlight-mfa-users',
+		'forced-mfa-users',
+		'inactive-users',
+		'session-control',
+		'xml-rpc',
+	],
+	available_modules: [
+		'inactive-users',
+		'highlight-mfa-users',
+		'forced-mfa-users',
+		'xml-rpc',
+		'session-control',
+		'notify-privileged-activity',
+	],
+	module_configs: {
+		'inactive-users': {
+			mode: 'BLOCK',
+			considered_inactive_after_days: 14,
+			roles: [],
+			capabilities: [ 'manage_options', 'edit_others_posts', 'publish_posts' ],
+			needs_review: false,
+		},
+		'forced-mfa-users': {
+			roles: [],
+			capabilities: [
+				'manage_options',
+				'edit_others_posts',
+				'publish_posts',
+				'edit_posts',
+			],
+			needs_review: false,
+		},
+		'xml-rpc': {
+			mode: 'DISABLE',
+			needs_review: false,
+		},
+		'session-control': {
+			expiration_days: 7,
+			needs_review: false,
+		},
+		'highlight-mfa-users': {
+			roles: [],
+			capabilities: [ 'manage_options', 'edit_others_posts' ],
+		},
+		'notify-privileged-activity': {
+			email_users: [],
+		},
+	},
+};
+export function getSecurityBoostConfig(
+	overrides?: Partial<SecurityBoostConfig>,
+): SecurityBoostConfig {
+	return {
+		...DEFAULT_CONFIG,
+		...overrides,
+		module_configs: {
+			...DEFAULT_CONFIG.module_configs,
+			...overrides?.module_configs,
+		},
+		enabled_modules: overrides?.enabled_modules ?? DEFAULT_CONFIG.enabled_modules,
+		available_modules:
+			overrides?.available_modules ?? DEFAULT_CONFIG.available_modules,
+	};
+}
+
+export function getSecurityBoostConfigHeaders(
+	partialConfig?: Partial<SecurityBoostConfig>,
+): { [key: string]: string } {
+	return {
+		'X-Integration-Test': 'true',
+		'X-Integration-Test-Configs': Buffer.from(
+			JSON.stringify( getSecurityBoostConfig( partialConfig ) ),
+			'utf8',
+		).toString( 'base64' ),
+	};
+}
--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -3,6 +3,8 @@
  */
 import { PlaywrightTestConfig } from '@playwright/test';
 
+import { getSecurityBoostConfigHeaders } from './lib/security-boost-config-helper';
+
 const config: PlaywrightTestConfig = {
 	retries: 1,
 	globalSetup: require.resolve( './lib/global-setup' ),
@@ -20,59 +22,7 @@ const config: PlaywrightTestConfig = {
 		baseURL: process.env.E2E_BASE_URL
 			? process.env.E2E_BASE_URL
 			: 'http://e2e-sb-test-site.vipdev.lndo.site',
-		extraHTTPHeaders: {
-			'X-Integration-Test': 'true',
-			'X-Integration-Test-Configs': Buffer.from(
-				JSON.stringify( {
-					enabled_modules: [
-						'notify-privileged-activity',
-						'highlight-mfa-users',
-						'forced-mfa-users',
-						'inactive-users',
-						'session-control',
-						'xml-rpc',
-					],
-					available_modules: [
-						'inactive-users',
-						'highlight-mfa-users',
-						'forced-mfa-users',
-						'xml-rpc',
-						'session-control',
-						'notify-privileged-activity',
-					],
-					module_configs: {
-						'inactive-users': {
-							mode: 'BLOCK',
-							considered_inactive_after_days: 14,
-							roles: [],
-							capabilities: [ 'manage_options', 'edit_others_posts', 'publish_posts' ],
-							needs_review: false,
-						},
-						'forced-mfa-users': {
-							roles: [],
-							capabilities: [ 'manage_options', 'edit_others_posts', 'publish_posts', 'edit_posts' ],
-							needs_review: false,
-						},
-						'xml-rpc': {
-							mode: 'DISABLE',
-							needs_review: false,
-						},
-						'session-control': {
-							expiration_days: 7,
-							needs_review: false,
-						},
-						'highlight-mfa-users': {
-							roles: [],
-							capabilities: [ 'manage_options', 'edit_others_posts' ],
-						},
-						'notify-privileged-activity': {
-							email_users: [],
-						},
-					},
-				} ),
-				'utf8',
-			).toString( 'base64' ),
-		},
+		extraHTTPHeaders: getSecurityBoostConfigHeaders(),
 	},
 };
 
