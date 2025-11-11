7795404abe59be07fc0788c24ca0e7f3aa9e1cb3
Andrea Grassi <andrea.grassi@automattic.com>
2025-11-11 10:49:53 +0100

    fix lint

--- modules/data-sync/class-data-sync.php
+++ modules/data-sync/class-data-sync.php
@@ -74,7 +74,7 @@ class Data_Sync {
 			'has_enable_two_factor_filter'           => \has_filter( 'wpcom_vip_enable_two_factor' ) !== false,
 
 			'has_mfa_additional_capabilities_filter' => $has_additional_capabilities_filter,
-			'mfa_additional_required_capabilities'    => $has_additional_capabilities_filter ? implode( ',', apply_filters( Forced_MFA_Users::ADDITIONAL_CAPABILITIES_FILTER_NAME, [] ) ) : '',
+			'mfa_additional_required_capabilities'   => $has_additional_capabilities_filter ? implode( ',', apply_filters( Forced_MFA_Users::ADDITIONAL_CAPABILITIES_FILTER_NAME, [] ) ) : '',
 		];
 		return $data;
 	}
